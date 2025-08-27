efb5356818417303851b6a5b4794c647b7b95e3f
Ali Miri <miri03369@gmail.com>
2025-08-27 12:14:24 +0330

    add bank name

--- finance/models.py
+++ finance/models.py
@@ -4,9 +4,26 @@ import uuid
 from django.conf import settings
 from django.utils import timezone
 from django.db import models
+
 from finance.utils import zpal_request_handler, zpal_payment_checker
 
 
 
+class Gateway(models.Model):
 
+    
+    FUNCTION_SAMAN = 'saman'
+    FUNCTION_SHAPARAK = 'shaparak'
+    FUNCTION_FINOTECH = 'fintech'
+    FUNCTION_ZARINPAL = 'zarinpal'
+    FUNCTION_PARSIAN = 'parsian'
+    FUNCTION_SAMAN = 'saman'
+    GATWAY_FUNCTIANS = (
+        (FUNCTION_SAMAN, -('saman')),
+        (FUNCTION_SHAPARAK, -('shaparak')),
+        (FUNCTION_FINOTECH, -('fintech')),
+        (FUNCTION_ZARINPAL, -('zarinpal')),
+        (FUNCTION_PARSIAN, -('parsian')),
+        (FUNCTION_SAMAN, -('saman')),
+    )
 
