0ca52919aef490c0c31e0f2a309514f77b055a3f
Ali Miri <miri03369@gmail.com>
2025-08-29 05:04:47 +0330

    creat purchase app

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from package.admin import PackageModelAdmin
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, related_name='purchases', on_delete=models.SET_NULL, null=True )
+    package = models.ForeignKey(PackageModelAdmin, related_name='purchases', on_delete=models.SET_NULL, null=True )
