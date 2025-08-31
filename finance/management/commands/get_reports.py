771e9149663fd35f3cd55ce3ecc14bd372bcc604
Ali Miri <miri03369@gmail.com>
2025-08-31 17:58:44 +0330

    creat custom commands

---
+++ finance/management/commands/get_reports.py
@@ -0,0 +1,13 @@
+from itertools import count
+
+from django.core.management import BaseCommand
+from django.db.models import Sum, Count
+
+from finance.models import Payment
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        payment = Payment.objects.aggregate(total=Sum('amount'), count=Count('id'))
+        print(payment)
