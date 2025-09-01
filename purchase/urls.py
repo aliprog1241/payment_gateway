c2204649014a0bd7e7e647732c154ff30ca30d8b
Ali Miri <miri03369@gmail.com>
2025-09-01 13:17:54 +0330

    creat purchase class

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from purchase.views import PurchaseCrateView
+from purchase.views import PurchaseCrateView, PurchaseListView
 
 urlpatterns = [
 
     path('creat/<int:package_id>/',  PurchaseCrateView.as_view(), name='create-purchase'),
+    path('list/',  PurchaseListView.as_view(), name='list-purchase'),
 
 ]
