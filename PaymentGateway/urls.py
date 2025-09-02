d3bdb5d568da3d98a16e31b57bc1873a29f61cbc
Ali Miri <miri03369@gmail.com>
2025-09-02 18:57:40 +0330

    edait html code

--- PaymentGateway/urls.py
+++ PaymentGateway/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('finance/', include('finance.urls')),
-    path('package/', include('package.urls')),
-    path('purchase/', include('purchase.urls')),
+    path("admin/", admin.site.urls),
+    path("finance/", include("finance.urls")),
+    path("package/", include("package.urls")),
+    path("purchase/", include("purchase.urls")),
 ]
